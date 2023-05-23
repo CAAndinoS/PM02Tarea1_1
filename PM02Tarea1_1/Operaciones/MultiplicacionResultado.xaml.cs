7596e457280b2952da2cf5bd0bc5efc3d6e3ef01
CAAndinoS <124225358+CAAndinoS@users.noreply.github.com>
2023-05-23 16:14:29 -0600

    Agregar archivos de proyecto.

---
+++ PM02Tarea1_1/Operaciones/MultiplicacionResultado.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM02Tarea1_1.Operaciones
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MultiplicacionResultado : ContentPage
+    {
+        public MultiplicacionResultado(String r)
+        {
+            InitializeComponent();
+            multi.Text = "El resultado de la Multiplicación es: " + r;
+        }
+    }
+}
